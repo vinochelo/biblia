ad5cb29c1ade78aefef4239e9243ee9d64a68440
Rocku <vicio86.vicio@gmail.com>
2025-11-10 19:59:06 +0000

    deja solo versiones en español

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,6 +4,6 @@ export const bibleVersions: BibleVersion[] = [
   { id: '592420522e16049f-01', name: 'Reina Valera 1909', abbreviation: 'RV1909' },
   { id: '6b7f504f1b6050c1-01', name: 'Nueva Biblia Viva', abbreviation: 'NBV' },
   { id: '48acedcf8595c754-01', name: 'Palabra de Dios para Ti', abbreviation: 'PDT' },
-  { id: 'b32b9d1b64b4ef29-01', name: 'The Holy Bible, Simple Spanish', abbreviation: 'SSE' },
+  { id: 'b32b9d1b64b4ef29-01', name: 'Santa Biblia, Español Sencillo', abbreviation: 'SSE' },
   { id: '482ddd53705278cc-02', name: 'Versión Biblia Libre', abbreviation: 'VBL' },
 ];
