63a4c7f985810625ea44e453cce961eb1de5b6eb
Rocku <vicio86.vicio@gmail.com>
2025-11-10 19:52:16 +0000

    al ver la biblia nvi da este error: Error Error de la API: You are not a

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,11 +1,11 @@
 import type { BibleVersion } from '@/lib/types';
 
 export const bibleVersions: BibleVersion[] = [
-  { id: 'de4e12af7f28f599-01', name: 'Reina Valera 1960', abbreviation: 'RVR1960' },
+  { id: 'c66e96957a4033b9-01', name: 'Reina Valera 1960', abbreviation: 'RVR1960' },
   { id: '06125adad2d5898a-01', name: 'King James Version', abbreviation: 'KJV' },
   { id: 'bba9f40183526463-01', name: 'La Biblia de las Américas', abbreviation: 'LBLA' },
-  { id: '592420522e16049f-01', name: 'Nueva Traducción Viviente', abbreviation: 'NTV' },
-  { id: '9879dbb6c895849c-01', name: 'Nueva Versión Internacional', abbreviation: 'NVI' },
+  { id: 'd38a5770df334894-01', name: 'Nueva Traducción Viviente', abbreviation: 'NTV' },
+  { id: '592420522e16049f-01', name: 'Nueva Versión Internacional', abbreviation: 'NVI' },
   { id: 'c315fa9f71d4af3a-01', name: 'American Standard Version', abbreviation: 'ASV' },
   { id: 'f72b840c855f362c-01', name: 'Biblia NTLH', abbreviation: 'NTLH'},
 ];
