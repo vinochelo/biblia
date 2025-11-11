4f5b5f5ccfca7ae8e901ed8b723d4ec5816062ee
Rocku <vicio86.vicio@gmail.com>
2025-11-11 12:21:33 +0000

    descarga tambien esta version: 1904 Patriarchal Greek New Testament with

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,4 +6,5 @@ export const bibleVersions: BibleVersion[] = [
   { id: '48acedcf8595c754-01', name: 'Palabra de Dios para Ti', abbreviation: 'PDT' },
   { id: 'b32b9d1b64b4ef29-01', name: 'Santa Biblia, Español Sencillo', abbreviation: 'SSE' },
   { id: '482ddd53705278cc-02', name: 'Versión Biblia Libre', abbreviation: 'VBL' },
+  { id: '901dcd9744e1bf69-01', name: 'Patriarchal Greek New Testament (1904)', abbreviation: 'PGT' },
 ];
