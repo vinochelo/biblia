fd3ac0b70f40df0c673ae504165f4289c40fe5e2
Rocku <vicio86.vicio@gmail.com>
2025-11-11 12:22:40 +0000

    y revisa que esten todas estas versiones con sus nombres: Biblica® Open

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,9 +2,11 @@ import type { BibleVersion } from '@/lib/types';
 
 export const bibleVersions: BibleVersion[] = [
   { id: '592420522e16049f-01', name: 'Reina Valera 1909', abbreviation: 'RV1909' },
-  { id: '6b7f504f1b6050c1-01', name: 'Nueva Biblia Viva', abbreviation: 'NBV' },
-  { id: '48acedcf8595c754-01', name: 'Palabra de Dios para Ti', abbreviation: 'PDT' },
-  { id: 'b32b9d1b64b4ef29-01', name: 'Santa Biblia, Español Sencillo', abbreviation: 'SSE' },
+  { id: '6b7f504f1b6050c1-01', name: 'Biblica® Open Nueva Biblia Viva 2008', abbreviation: 'NBV' },
+  { id: '48acedcf8595c754-01', name: 'Spanish Bible, Palabla de Dios para ti', abbreviation: 'PDT' },
+  { id: '48acedcf8595c754-02', name: 'Spanish NT + PP, Palabla de Dios para ti', abbreviation: 'PDT-NT' },
+  { id: 'b32b9d1b64b4ef29-01', name: 'The Holy Bible in Simple Spanish', abbreviation: 'SSE' },
+  { id: '482ddd53705278cc-01', name: 'The New Testament in Spanish, Free Bible Version', abbreviation: 'VBL-NT' },
   { id: '482ddd53705278cc-02', name: 'Versión Biblia Libre', abbreviation: 'VBL' },
   { id: '901dcd9744e1bf69-01', name: 'Patriarchal Greek New Testament (1904)', abbreviation: 'PGT' },
 ];
