7a3d6df06dd8138283986c0e452c160a45fa108e
Rocku <vicio86.vicio@gmail.com>
2026-02-12 17:59:22 +0000

    en español solo dejas estas versiones

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,15 +2,8 @@
 import type { BibleVersion } from '@/lib/types';
 
 export const bibleVersions: BibleVersion[] = [
-  { id: '592420522e16049f-01', name: 'Reina Valera 1909', abbreviation: 'RV1909' },
-  { id: '6b7f504f1b6050c1-01', name: 'Biblica® Open Nueva Biblia Viva 2008', abbreviation: 'NBV' },
-  { id: '48acedcf8595c754-01', name: 'Spanish Bible, Palabla de Dios para ti', abbreviation: 'PDT' },
-  { id: '48acedcf8595c754-02', name: 'Spanish NT + PP, Palabla de Dios para ti', abbreviation: 'PDT-NT' },
-  { id: 'b32b9d1b64b4ef29-01', name: 'The Holy Bible in Simple Spanish', abbreviation: 'SSE' },
-  { id: '482ddd53705278cc-01', name: 'The New Testament in Spanish, Free Bible Version', abbreviation: 'VBL-NT' },
+  { id: '592420522e16049f-01', name: 'Reina Valera 1909', abbreviation: 'RVR09' },
+  { id: 'b32b9d1b64b4ef29-01', name: 'Biblia en Español Sencillo', abbreviation: 'SSE' },
+  { id: '48acedcf8595c754-01', name: 'Palabra de Dios para Todos', abbreviation: 'PDT' },
   { id: '482ddd53705278cc-02', name: 'Versión Biblia Libre', abbreviation: 'VBL' },
-  { id: '901dcd9744e1bf69-01', name: 'Patriarchal Greek New Testament (1904)', abbreviation: 'PGT' },
-  { id: 'b833918a54be2f62-01', name: 'Biblia en audio dramatizado en español latino', abbreviation: 'NVI-A' },
 ];
-
-    
