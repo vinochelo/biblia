764c296992b7b77ff3304d85b7ade64a0eecc5a3
Rocku <vicio86.vicio@gmail.com>
2026-02-12 18:02:18 +0000

    añade en hebreo esta version

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,3 @@
-
 import type { BibleVersion } from '@/lib/types';
 
 export const bibleVersions: BibleVersion[] = [
@@ -6,4 +5,5 @@ export const bibleVersions: BibleVersion[] = [
   { id: 'b32b9d1b64b4ef29-01', name: 'Biblia en Español Sencillo', abbreviation: 'SSE' },
   { id: '48acedcf8595c754-01', name: 'Palabra de Dios para Todos', abbreviation: 'PDT' },
   { id: '482ddd53705278cc-02', name: 'Versión Biblia Libre', abbreviation: 'VBL' },
+  { id: '0b262f1ed7f084a6-01', name: 'Hebrew Bible: Westminster Leningrad Codex', abbreviation: 'hboWLC' },
 ];
